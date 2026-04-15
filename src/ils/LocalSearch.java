9c90aff5b6ca4648429d93a96f02992fed80d0ed
MissNcube <aundreancu@gmail.com>
2026-04-15 11:11:43 +0200

    Base code for ils, i am still coding the methods.

--- src/ils/LocalSearch.java
+++ src/ils/LocalSearch.java
@@ -1,5 +1,23 @@
 package ils;
 
+import common.KnapsackInstance;
+import common.Solution;
+
 public class LocalSearch {
     
+    /* public Solution run(Solution curr, KnapsackInstance inst)
+    {
+
+    } */
+
+    void repair(Solution s, KnapsackInstance inst)
+    {
+
+        while(!s.isValid(inst))
+        {
+            //remove random item until valid
+        }
+    }
+
+
 }
--- src/ils/iteratedLocalSearch.java
+++ src/ils/iteratedLocalSearch.java
@@ -1,5 +1,26 @@
 package ils;
+import common.Solution;
+import java.util.Random;
 
 public class iteratedLocalSearch {
-    
+    private static int max_non_improving = 500; //stopping criterion of no improvement after 500 iter
+    private static final int perturbation_strength = 4; //number of random flips in perturbation
+
+    /* public Solution run(KnapsackInstance inst, Random rng) {
+        
+    }
+
+    private Solution greedyConstruct(KnapsackInstance inst, Random rng)
+    {
+
+    } */
+
+    public static int getMaxNoImprove(){
+        return max_non_improving;
+    }
+
+    public static int getPerturbationk(){
+        return perturbation_strength;
+    }
+
 }
